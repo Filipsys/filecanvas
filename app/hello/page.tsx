580f6c0fe15f93e99e61cf39dda40e9ce24a53d2
Filipsys <filipsysak@gmail.com>
2024-07-06 23:14:27 +0200

    chore: Update package dependencies and add CSS styles for home page

---
+++ app/hello/page.tsx
@@ -0,0 +1,7 @@
+export default function Hello() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="font-bold text-3xl">Hello world!</h1>
+    </div>
+  );
+}

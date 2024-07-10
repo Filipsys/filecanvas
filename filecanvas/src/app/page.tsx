19f87469f8b5ad83798ee1c11659594868e067bf
Filipsys <filipsysak@gmail.com>
2024-07-10 20:19:34 +0200

    chore: Refactor page styles and update navigation icons

--- filecanvas/src/app/page.tsx
+++ filecanvas/src/app/page.tsx
@@ -9,50 +9,50 @@ import link from "../../public/navicons/link.svg";
 import pan from "../../public/navicons/pan.svg";
 import resize from "../../public/navicons/resize.svg";
 
-import styles from "../../styles/Page.module.css";
+import styles from "./Page.module.css";
 
 
 export default async function Home() {
   const elementsJSON = await db.select().from(elements);
 
   const containers = elementsJSON.map((element) => (
-    <div className="" style={{ width: element.width, height: element.height, top: element.y, left: element.x, backgroundColor: "#0e4145", position: "absolute", border: "3px solid #12585d", borderRadius: "10px", padding: "10px" }} key={element.id}>
+    <div className="" style={{ width: element.width || "fit-content", height: element.height, top: element.y, left: element.x, backgroundColor: "#0e4145", position: "absolute", border: "3px solid #12585d", borderRadius: "10px", padding: "10px" }} key={element.id}>
       <div style={{ fontSize: "20px", color: "white", verticalAlign: "top", textAlign: "left", userSelect: "none" }}>
-        {element.data}
+        {element.data ? element.data : element.dataLink}
       </div>
     </div>
   ));
 
   return (
-    <main className={styles.page}>
+    <main className={styles.pageeee}>
       <nav className={styles.navbarContainer}>
-        <div className="navbar">
-          <div className="navbar-center">
-            <div className="navbar-item">
-              <Image alt="pan" src={pan} width={20} height={20} className="item-image" />
+        <div className={styles.navbar}>
+          <div className={styles.navbarCenter}>
+            <div className={styles.navbarItem}>
+              <Image alt="pan" src={pan}  className={styles.itemImage} />
             </div>
-            <div className="navbar-item">
-              <Image alt="resize" src={resize} width={20} height={20} style={{ borderRadius: 7, opacity: .7, padding: "5px", backgroundColor: "#31303b" }} />
+            <div className={styles.navbarItem}>
+              <Image alt="resize" src={resize}  className={styles.itemImage} />
             </div>
-            <div className="navbar-item">
-              <Image alt="edit" src={edit} width={20} height={20} className="item-image" />
+            <div className={styles.navbarItem}>
+              <Image alt="edit" src={edit}  className={styles.itemImage} />
             </div>
-            <div className="navbar-item">
-              <Image alt="image" src={image} width={20} height={20} className="item-image" />
+            <div className={styles.navbarItem}>
+              <Image alt="image" src={image}  className={styles.itemImage} />
             </div>
-            <div className="navbar-item">
-              <Image alt="link" src={link} width={20} height={20} className="item-image" />
+            <div className={styles.navbarItem}>
+              <Image alt="link" src={link} style={{ transform: "rotate(-45deg)" }} className={styles.itemImage} />
             </div>
-            <div className="navbar-item">
-              <Image alt="article" src={article} width={20} height={20} className="item-image" />
+            <div className={styles.navbarItem}>
+              <Image alt="article" src={article}  className={styles.itemImage} />
             </div>
           </div>
         </div>
-      </nav>
+      </nav> 
       
       {containers}
 
-      {/* <div style={{ width: "150vw", height: "150vh", backgroundImage: "radial-gradient(circle, rgba(255, 255, 255, 0.02) 10%, transparent 10%)", backgroundSize: "30px 30px" }} /> */}
+      <div style={{ width: "150vw", height: "150vh", backgroundImage: "radial-gradient(circle, rgba(255, 255, 255, 0.02) 10%, transparent 10%)", backgroundSize: "30px 30px", overflow: "hidden" }} />
     </main>
   );
 }

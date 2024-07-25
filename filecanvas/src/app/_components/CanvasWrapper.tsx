745dbb8e6eb4f4fcbf29bc98298a6422a9b742f0
Filipsys <filipsysak@gmail.com>
2024-07-25 19:59:44 +0200

    tRPC implementation, still in progress

---
+++ filecanvas/src/app/_components/CanvasWrapper.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useState } from "react";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import type { AppRouter } from "@/server";
+
+import "../../../styles/elements.css";
+
+const icons = {
+  move: <div className="invisible group-hover:visible"></div>,
+};
+
+const client = createTRPCClient<AppRouter>({
+  links: [httpBatchLink({ url: "http://localhost:3333" })],
+});
+
+export function CanvasIconWrapper(props: { mode: "move" | null }) {
+  const [isHovering, setIsHovering] = useState(false);
+  return (
+    <div
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+      style={{
+        width: "100px",
+        height: "100px",
+        backgroundColor: "red",
+        position: "absolute",
+        top: "100px",
+        left: "100px",
+      }}
+    >
+      {/* ... stuff */}
+      {isHovering && "hovering"}
+      {!isHovering && "not hovering"}
+
+      {props.mode && icons[props.mode]}
+    </div>
+  );
+}
+
+export function MyCanvasElement(props: {
+  mode: "move" | null;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  data: string | null;
+  dataLink: string | null;
+}) {
+  return (
+    <div
+      className="group"
+      style={{
+        width: props.width || "fit-content",
+        height: props.height,
+        top: props.y,
+        left: props.x,
+        backgroundColor: "#0e4145",
+        position: "absolute",
+        border: "3px solid #12585d",
+        borderRadius: "10px",
+        padding: "10px",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "20px",
+          color: "white",
+          verticalAlign: "top",
+          textAlign: "left",
+          userSelect: "none",
+        }}
+      >
+        {props.data ? props.data : props.dataLink}
+      </div>
+
+      {props.mode && icons[props.mode]}
+
+      <button
+        onClick={async () => {
+          const result = await client.elementList.query();
+
+          console.log(result);
+        }}
+      >
+        test
+      </button>
+    </div>
+  );
+}
---
+++ filecanvas/src/server/routers/_app.ts
@@ -0,0 +1,33 @@
+import { db } from "@/database/db";
+import { elements } from "@/database/schema";
+import { publicProcedure, router } from "@/server/trpc";
+import { z } from "zod";
+
+export const appRouter = router({
+  elementList: publicProcedure.query(async () => {
+    const output = await db.select().from(elements);
+    return output;
+  }),
+  elementCreate: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        x: z.number(),
+        y: z.number(),
+        width: z.number(),
+        height: z.number(),
+        dataType: z.string(),
+        dataLink: z.string(),
+        data: z.string(),
+        color: z.string(),
+      }),
+    )
+    .mutation(async (opts) => {
+      const { input } = opts;
+
+      const output = await db.insert(elements).values(input);
+      return output;
+    }),
+});
+
+export type AppRouter = typeof appRouter;
